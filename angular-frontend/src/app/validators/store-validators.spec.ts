b1bc011e7278bb37505f66c979a40d5f829d3a9c
seanjungmsba <seanjeong1996@gmail.com>
2022-09-07 18:03:35 -0400

    store-validators added

---
+++ angular-frontend/src/app/validators/store-validators.spec.ts
@@ -0,0 +1,7 @@
+import { StoreValidators } from './store-validators';
+
+describe('Luv2ShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new StoreValidators()).toBeTruthy();
+  });
+});
