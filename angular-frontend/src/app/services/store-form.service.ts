d768cb34f43a20240ca80afe96de1709c4eb91ea
seanjungmsba <seanjeong1996@gmail.com>
2022-09-07 18:13:00 -0400

    store-form service updated

--- angular-frontend/src/app/services/store-form.service.ts
+++ angular-frontend/src/app/services/store-form.service.ts
@@ -36,9 +36,9 @@ export class StoreFormService {
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
     let data: number[] = [];
-
+    
     // build an array for "Month" dropdown list
-    // - start at current month and loop until
+    // - start at current month and loop until 
 
     for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
       data.push(theMonth);
@@ -53,7 +53,7 @@ export class StoreFormService {
 
     // build an array for "Year" downlist list
     // - start at current year and loop for next 10 years
-
+    
     const startYear: number = new Date().getFullYear();
     const endYear: number = startYear + 10;
 
