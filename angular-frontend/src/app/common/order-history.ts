e91c05b0b44eba87dc4d07352848d6a14c95393c
seanjungmsba <seanjeong1996@gmail.com>
2022-09-08 13:59:53 -0400

    order-history class created

---
+++ angular-frontend/src/app/common/order-history.ts
@@ -0,0 +1,7 @@
+export class OrderHistory {
+    id: string = '';
+    orderTrackingNumber: string = '';
+    totalPrice: number = 0;
+    totalQuantity: number = 0;
+    dateCreated: Date = new Date();
+}
