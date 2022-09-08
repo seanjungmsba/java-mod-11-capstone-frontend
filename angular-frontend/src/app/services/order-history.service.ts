073f97978b11fad1bcd9c28ffbe19b76ee718b3e
seanjungmsba <seanjeong1996@gmail.com>
2022-09-08 18:11:37 -0400

    order-history service bug fixed

--- angular-frontend/src/app/services/order-history.service.ts
+++ angular-frontend/src/app/services/order-history.service.ts
@@ -8,14 +8,14 @@ import { OrderHistory } from '../common/order-history';
 })
 export class OrderHistoryService {
 
-  private orderUrl = 'http://localhost:8080/api/order';
+  private orderUrl = 'http://localhost:8080/api/orders';
 
   constructor(private httpClient: HttpClient) { }
 
   getOrderHistory(email: string): Observable<GetResponseOrderHistory> {
 
-    //build URL based on the customer email
-    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=$email=${email}`;
+    // build URL based on the customer email
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=${email}`;
 
     // return the REST API object
     return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
