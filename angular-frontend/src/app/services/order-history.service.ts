df966ae76d064922a8047125e6c734ead516e334
seanjungmsba <seanjeong1996@gmail.com>
2022-09-08 14:00:06 -0400

    order-history service created

---
+++ angular-frontend/src/app/services/order-history.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = 'http://localhost:8080/api/order';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOrderHistory(email: string): Observable<GetResponseOrderHistory> {
+
+    //build URL based on the customer email
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=$email=${email}`;
+
+    // return the REST API object
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
+  }
+}
+
+interface GetResponseOrderHistory {
+  _embedded: {
+    orders: OrderHistory[];
+  }
+}
