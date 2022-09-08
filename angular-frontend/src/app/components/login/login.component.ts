b41e03b865e72dd00ab99fd11a93be047b2311cf
seanjungmsba <seanjeong1996@gmail.com>
2022-09-07 22:42:57 -0400

    login component created

---
+++ angular-frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaSignIn from '@okta/okta-signin-widget';
+import AppConfig from '../../config/app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: AppConfig.oidc.issuer.split('/oauth')[0],
+      clientId: AppConfig.oidc.clientId,
+      redirectUri: AppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: AppConfig.oidc.issuer,
+        scopes: AppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-frontend/src/app/config/app-config.ts
+++ angular-frontend/src/app/config/app-config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-        clinetId: '0oa6fho85jk3dQQQv5d7',
+        clientId: '0oa6fho85jk3dQQQv5d7',
         issuer: 'https://dev-38035794.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
