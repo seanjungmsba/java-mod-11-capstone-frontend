22ecf6fc2032a15b79928b0bdf69c5556883693f
seanjungmsba <seanjeong1996@gmail.com>
2022-09-07 22:54:50 -0400

    login-status component created

---
+++ angular-frontend/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,46 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+import {OktaAuthStateService, OKTA_AUTH} from '@okta/okta-angular';
+import {OktaAuth} from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+
+  isAuthentificated: boolean = false;
+  userFullName: string = '';
+
+  constructor(private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  ngOnInit(): void {
+    // subscribe to authentification state changes
+    this.oktaAuthService.authState$.subscribe(
+      (result) => {
+        this.isAuthentificated = result.isAuthenticated!;
+        this.getUserDetails();
+      }
+    )
+  }
+
+  getUserDetails() {
+    if (this.isAuthentificated) {
+      // fetch the logged-in user details (user's claims)
+      // user full name is exposed as a property name
+      this.oktaAuth.getUser().then(
+        (res) => {
+          this.userFullName = res.name as string;
+        }
+      );
+    }
+  }
+
+  logout() {
+    // terminates the session with Okta and removes current tokens
+    this.oktaAuth.signOut();
+  }
+
+}
