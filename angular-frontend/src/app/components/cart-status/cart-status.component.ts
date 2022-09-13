872fed5368463fc4d7bf9ca9ea37ba77a86ce512
seanjungmsba <seanjeong1996@gmail.com>
2022-09-13 19:25:55 -0400

    documentation added to cart-status component

--- angular-frontend/src/app/components/cart-status/cart-status.component.ts
+++ angular-frontend/src/app/components/cart-status/cart-status.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from 'src/app/services/cart.service';
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
-  selector: 'app-cart-status',
-  templateUrl: './cart-status.component.html',
-  styleUrls: ['./cart-status.component.css']
+  selector: "app-cart-status",
+  templateUrl: "./cart-status.component.html",
+  styleUrls: ["./cart-status.component.css"],
 })
+/* Cart Status component is responsible populating the data on cart icon located top right */ 
 export class CartStatusComponent implements OnInit {
 
-  totalPrice: number = 0.00;
+  // initialize total price and quantity of the cart
+  totalPrice: number = 0.0;
   totalQuantity: number = 0;
 
-  constructor(private cartService: CartService) { }
+  // dependency injection
+  constructor(private cartService: CartService) {}
 
+  // call updateCartStatus() method when app starts up 
   ngOnInit(): void {
     this.updateCartStatus();
   }
 
+  /* updateCartStatus() tells CartService to update totalPrice and totalQuatity value accordingly */
   updateCartStatus() {
-
     // subscribe to the cart totalPrice
-    this.cartService.totalPrice.subscribe(
-      data => this.totalPrice = data
-    );
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
 
     // subscribe to the cart totalQuantity
     this.cartService.totalQuantity.subscribe(
-      data => this.totalQuantity = data
+      (data) => (this.totalQuantity = data)
     );
-
   }
-
 }
