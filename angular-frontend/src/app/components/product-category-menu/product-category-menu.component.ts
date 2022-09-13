e17e3442df8ab45b650bb62bf17e8ce3dbe1e20c
seanjungmsba <seanjeong1996@gmail.com>
2022-09-13 19:13:12 -0400

    documentation added to product-category-menu component

--- angular-frontend/src/app/components/product-category-menu/product-category-menu.component.ts
+++ angular-frontend/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductCategory } from 'src/app/common/product-category';
-import { ProductService } from 'src/app/services/product.service';
+import { Component, OnInit } from "@angular/core";
+import { ProductCategory } from "src/app/common/product-category";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-product-category-menu',
-  templateUrl: './product-category-menu.component.html',
-  styleUrls: ['./product-category-menu.component.css']
+  selector: "app-product-category-menu",
+  templateUrl: "./product-category-menu.component.html",
+  styleUrls: ["./product-category-menu.component.css"],
 })
+/*
+ * Product Category Menu component is responsible for UI with regards to 
+ * menu bar located in the left side that displays different meal categories
+ */
 export class ProductCategoryMenuComponent implements OnInit {
 
+  // initialize empty array of ProductCategory
   productCategories: ProductCategory[] = [];
-  
-  constructor(private productService: ProductService) { }
 
+  // dependency injection
+  constructor(private productService: ProductService) {}
+
+  // call listProductCategories() when application starts up
   ngOnInit() {
     this.listProductCategories();
   }
 
+  /* listProductCategories() tells ProductService to retrieve 
+   * the list of product categories and assign them to the productCategories array 
+   */ 
   listProductCategories() {
-
-    this.productService.getProductCategories().subscribe(
-      data => {
-        console.log('Product Categories=' + JSON.stringify(data));
-        this.productCategories = data;
-      }
-    );
+    this.productService.getProductCategories().subscribe((data) => {
+      console.log("Product Categories=" + JSON.stringify(data));
+      this.productCategories = data;
+    });
   }
-
 }
