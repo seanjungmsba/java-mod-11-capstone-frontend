c24fdbbef10146ca2b8f52a4a1a2319f93725b5e
seanjungmsba <seanjeong1996@gmail.com>
2022-09-13 19:18:58 -0400

    documentation added to search component

--- angular-frontend/src/app/components/search/search.component.ts
+++ angular-frontend/src/app/components/search/search.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
 })
+/* Search component is responsible for the search bar on the top
+ * and locates product based on the keyword that is being search for */
 export class SearchComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  // dependency injection
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  /* doSearch() tells Router to redirect url to keyword that user searches for */
   doSearch(value: string) {
     console.log(`value=${value}`);
     this.router.navigateByUrl(`/search/${value}`);
