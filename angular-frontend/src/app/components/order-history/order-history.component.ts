337078df8314e6a01b5c1fcb85dfc04ff730cf23
seanjungmsba <seanjeong1996@gmail.com>
2022-09-13 20:36:45 -0400

    documentation added to order-history component

--- angular-frontend/src/app/components/order-history/order-history.component.ts
+++ angular-frontend/src/app/components/order-history/order-history.component.ts
@@ -7,26 +7,31 @@ import { OrderHistoryService } from 'src/app/services/order-history.service';
   templateUrl: './order-history.component.html',
   styleUrls: ['./order-history.component.css']
 })
+/* Order History component is responsible for displaying order 
+ * data for the user who signed up and purchased items */
 export class OrderHistoryComponent implements OnInit {
 
+  // initialize empty array of OrderHistory
   orderHistoryList: OrderHistory[] = [];
 
   // reference to web browser's session storage
   storage: Storage = sessionStorage;
   
+  // dependency injection of OrderHistoryService
   constructor(private orderHistoryService: OrderHistoryService) { }
 
+  // call handleOrderHistory() when application starts up
   ngOnInit(): void {
     this.handleOrderHistory();
   }
 
+  /* handleOrderHistory() correctly updates theEmail field and orderHistoryList */
   handleOrderHistory() {
-    
     // read user's email address from browser storage
     // return null if it doesn't exist
     const theEmail = JSON.parse(this.storage.getItem('userEmail') || '{}');
 
-    // retrieve data from the service
+    // retrieve data from the service using user's email address
     this.orderHistoryService.getOrderHistory(theEmail).subscribe(
       data => {
         this.orderHistoryList = data._embedded.orders;
